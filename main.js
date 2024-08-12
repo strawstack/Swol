a46b0f362738e7721b0722f28206c1553a8390b9
RichardHayes <rhshadowgreen@gmail.com>
2024-08-12 11:03:18 -0700

    Implement push and pop functions. Write wasm prototype solution for AOC Day 1 2019

--- main.js
+++ main.js
@@ -6,42 +6,106 @@
     const wasmOut = document.querySelector(".wasm.output");
     const liftElem = document.querySelector(".lift");
     const outElem = document.querySelector(".mirror.output");
+    
+    // Get program input and wasm global for stack size
+    const weight = weightElem.innerHTML.split("\n").map(e => parseInt(e, 10)).reverse();
+    const stack_size = new WebAssembly.Global({ value: "i32", mutable: true }, weight.length);
 
-    const memory = new WebAssembly.Memory({ initial: 1 });
-    const data = new DataView(memory.buffer);
-    for (let i = 0; i < 10; i++) {
-        data.setInt32(i * 4, i, true);
-    }
+    // Init wasm memory
+    const memory = (() => {
+        const memory = new WebAssembly.Memory({ initial: 1 });
+        const data = new DataView(memory.buffer);
+        for (let i = 0; i < weight.length; i++) {
+            data.setInt32(i * 4, weight[i], true);
+        }
+        return memory;
+    })();
 
     const lift = async (e) => {
-        const { main } = await wasm({ imports: { memory } })`
+        const { main } = await wasm({ imports: { memory, stack_size } })`
         (module
 
             (import "imports" "memory" (memory 1))
             
-            (func $store (param i32) (param i32)
-                local.get 0
-                i32.const 8
+            (global $s (import "imports" "stack_size") (mut i32))
+
+            (func $store (param $index i32) (param $value i32)
+                local.get $index
+                i32.const 4
                 i32.mul
-                local.get 1
+                local.get $value
                 i32.store
             )
 
-            (func $load (param i32) (result i32)
-                local.get 0
-                i32.const 8
+            (func $load (param $index i32) (result i32)
+                local.get $index
+                i32.const 4
                 i32.mul
                 i32.load
             )
 
-            (func (export "main") (result i32)
-                
-                i32.const 3
-                i32.const 17
+            (func $push (param $value i32)
+                global.get $s
+                local.get $value
                 call $store
-                
-                i32.const 3
+                (i32.add (global.get $s) (i32.const 1))
+                global.set $s
+            )
+
+            (func $pop (result i32)
+                (i32.sub (global.get $s) (i32.const 1))    
+                global.set $s
+                global.get $s
                 call $load
+            )
+
+            (func $swap
+                (local $t0 i32)
+                (local $t1 i32)
+                
+                call $pop
+                local.set $t0
+                call $pop
+                local.set $t1
+                
+                local.get $t0
+                call $push
+                local.get $t1
+                call $push
+            )
+
+            (func (export "main") (result i32)
+
+                (local $count i32)
+                (local.set $count (i32.const 3))
+
+                call $pop
+                i32.const 3
+                i32.div_s
+                i32.const 2
+                i32.sub
+                call $push
+
+                loop $loop
+                    call $swap
+                    call $pop
+                    i32.const 3
+                    i32.div_s
+                    i32.const 2
+                    i32.sub
+                    call $pop
+                    i32.add
+                    call $push
+
+                    (local.set $count (i32.sub (local.get $count) (i32.const 1)))
+                    local.get $count
+                    i32.const 0
+                    i32.gt_s
+                    br_if $loop
+                end
+
+                call $pop                
+
             )
         )`;
 
